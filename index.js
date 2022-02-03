5e4088fcd39b6cd576635a14fbdfc98638fd9839
Justin Meddaugh <NdoeMusic@Justins-MacBook-Pro.local>
2022-02-02 20:46:21 -0600

    started the profile page

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 let content = document.querySelector('.js-generated.content')
+// create the header
+let header = document.createElement('h1')
+//add class dog-name
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+//append the header to the content
+content.append(header)
+//doggy image
+let doggyImg = document.querySelector('dog-image')
+doggyImg.setAttribute('dog-content','dog-image')
